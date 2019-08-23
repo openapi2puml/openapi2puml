798ede10b8ab52906c60e55396b54a408f93abf9
Darren Costello <darren.costello@gmail.com>
2019-08-24 01:04:50 +0200

    handle open api specs with multiple types of error responses e.g. 400, 404 etc

--- swagger2puml-core/src/test/java/com/kicksolutions/swagger/plantuml/PlantUMLGeneratorTest.java
+++ swagger2puml-core/src/test/java/com/kicksolutions/swagger/plantuml/PlantUMLGeneratorTest.java
@@ -1,10 +1,12 @@
 package com.kicksolutions.swagger.plantuml;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PlantUMLGeneratorTest {
@@ -47,4 +49,22 @@ class PlantUMLGeneratorTest {
     assertTrue(new File(outputPath + "/" + DEFAULT_PLANT_UML_FILENAME).exists(), "Expect PlantUML file to be generated");
   }
 
+  @Test
+  @DisplayName("Petstore test with Multiple Response Types")
+  void test_generatePetStorePlantUml_withMultipleResponseTypes() throws Exception{
+    String specFile = "src/test/resources/petstore_with_multipleErrorResponseMessages/swagger.yaml";
+    String outputPath = "src/test/resources/petstore_with_multipleErrorResponseMessages/";
+    boolean generateDefinitionModelOnly = false;
+    boolean includeCardinality = true;
+    boolean generateSvg = false; // TODO - this is to generate the svg image file but it doesn't work correctly as a
+    // test
+
+    generator.transformSwagger2Puml(specFile, outputPath, generateDefinitionModelOnly, includeCardinality, generateSvg);
+
+    File plantUmlFile = new File(outputPath + "/" + DEFAULT_PLANT_UML_FILENAME);
+    String stringToFind = "CatApi -->    ErrorMessage,WarningMessage";
+    assertTrue(plantUmlFile.exists(), "Expect PlantUML file to be generated");
+    assertFalse(FileUtils.readFileToString(plantUmlFile).contains(stringToFind), "Expect that invalid response "
+        + "realtion will NOT be generated");
+  }
 }
