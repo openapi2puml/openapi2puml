63b239647dea64948226a43b17546d0eb4d8a0b3
t.eitzenberger <thomas.eitzenberger@gematik.de>
2020-11-04 13:20:47 +0100

    reenabling output param in usage

--- openapi2puml-core/src/main/java/org/openapi2puml/openapi/OpenApi2PlantUML.java
+++ openapi2puml-core/src/main/java/org/openapi2puml/openapi/OpenApi2PlantUML.java
@@ -13,7 +13,7 @@ public class OpenApi2PlantUML {
       .append(" Usage: ")
       .append(OpenApi2PlantUML.class.getName()).append(" <options> \n")
       .append(" -i <spec file> ")
-      //.append(" -o <output directory> ")
+      .append(" -o <output directory> ")
       .append(" -generateDefinitionModelOnly true/false; Default=false ")
       .append(" -includeCardinality true/false; Default=true ")
           .append(" -generateSvg true/false; Default=true ")
